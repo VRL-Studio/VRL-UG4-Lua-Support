4e4a2f1bbcc93b506187774f0c8bc0a3b468fa05
turpid-monkey <s1496737@stud.uni-frankfurt.de>
2015-01-17 17:29:54 +0100

    Added dependency to luaautocompletion and ug4luaautocompletion artifacts from bintray Added RSyntaxTextArea integration classes

---
+++ src/test/java/edu/gcsc/uglua/UG4LuaAutoCompletionProviderTest.java
@@ -0,0 +1,16 @@
+package edu.gcsc.uglua;
+
+import org.junit.Test;
+
+import edu.gcsc.vrl.langsupport.ug4lua.UG4LuaAutoCompletionProvider;
+
+public class UG4LuaAutoCompletionProviderTest {
+
+	@Test
+	public void testInit() {
+		UG4LuaAutoCompletionProvider prov = new UG4LuaAutoCompletionProvider();
+		// List<Completion> l = prov.loadCompletions();
+	    // TODO check success
+	}
+
+}
